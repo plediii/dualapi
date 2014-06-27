631d2b5f03ce2e4d3ed4835d4da4b942248a8a42
Paul Ledbetter <plediii.atr@gmail.com>
2014-06-27 21:48:36 +0000

    Add actual example.

--- example.js
+++ example.js
@@ -1,3 +1,34 @@
 
 var xml = require('xml4node');
-var doc = xml.doc(); 
+var dual = require('./index');
+var _ = require('underscore');
+
+
+var exampleHost = function () {
+    var doc = xml.doc(xml.elt('bands'));
+
+    var host = new dual.Host();
+    host.action('get')
+        .route(['site'])
+        .use(function (route, msg, remote) {
+            remote.trigger('put', ['site'].concat(_.initial(route)), xml.nodeToString(xml.docGet(route)));
+        });
+
+    host.action('put')
+        .route(['site'])
+        .use(function (route, msg) {
+            xml.docSet(route, msg);
+        });
+
+    host.doc = doc;
+    return host;
+};
+
+var hostA = exampleHost();
+var hostB = exampleHost();
+
+hostA.serve(hostB);
+
+hostA.trigger('put', ['site', 'bands'], xml.elt('beatles'));
+hostB.emit('get', ['site', 'bands']);
+console.log(hostB.doc.root);
