88146498133f722bf565f99b055c96ea5eb41c00
Paul Ledbetter <plediii.atr@gmail.com>
2014-06-28 08:15:15 +0000

    Add dummy implementation.

---
+++ index.js
@@ -0,0 +1,50 @@
+
+var _ = require('underscore');
+var EventEmitter = require('events').EventEmitter;
+var inherits = require('util').inherits;
+
+var Host = function () {
+    var _this = this;
+    _this.actions = {};
+};
+
+inherits(Host, EventEmitter);
+
+_.extend(Host.prototype, {
+    action: function (name) {
+        var _this = this;
+        _this.actions[name] = _this.actions[name] || [];
+        var routes = _this.actions[name];
+        return {
+            route: function (route) {
+                return {
+                    use: function (cb) {
+                        routes.push([route, cb]);
+                    }
+                };
+            }
+        };
+    }
+    , trigger: function (name, route, msg, remote) {
+        var _this = this;
+        _.each(_this.actions[name], function (routecb) {
+            var route = routecb[0];
+            var cb = routecb[1];
+            if (_.deepEqual(route, target.slice(0, route.length))) {
+                cb(target.slice(route.length), msg, _this);
+            }
+        });
+    }
+    , serve: function (remote) {
+        var _this = this;
+        _.each(_this.actions, function (name) function () {
+            remote.on(name, function (target, msg) {
+                _this.trigger(name, route, msg, _this);
+            });
+        });
+    }
+});
+
+_.extend(exports, {
+    Host: Host
+});
