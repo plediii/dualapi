a0de5d507ecbcd5d33089239b69c8f963816fa42
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2014-09-24 14:29:16 -0500

    Add constraints on the options processing during .forward and .reply.

--- test/messageContext.js
+++ test/messageContext.js
@@ -9,7 +9,7 @@ describe('dualapi', function () {
 
     
     describe('messageContext', function () {
-
+        
         describe('.reply', function () {
 
             it('should send the argument to the message from host', function (done) {
@@ -17,26 +17,93 @@ describe('dualapi', function () {
                     domain: {
                         send: function (to, from, body) {
                             assert.deepEqual(to, ['deep', 'space', 'nine']);
-                            assert.deepEqual(body, { ferengi: 'attack'});
                             done();
                         }
                     }
                     , from: ['deep', 'space', 'nine']
                 });
-                ctxt.reply({ferengi: 'attack'});
+                ctxt.reply();
             });
+
+
+            it('should send with argument as body', function (done) {
+                var ctxt = new dualapi.MessageContext({
+                    domain: {
+                        send: function (to, from, body) {
+                            assert.deepEqual(body, { ferengi: 'attack'});
+                            done();
+                        }
+                    }
+                    , from: ['drain']
+                });
+                ctxt.reply({ ferengi: 'attack'});
+            });
+
+            it('should *not* include options', function (done) {
+                var ctxt = new dualapi.MessageContext({
+                    domain: {
+                        send: function (to, from, body, options) {
+                            assert(!options);
+                            done();
+                        }
+                    }
+                    , from: ['got', 'got', 'got']
+                    , options: {beautiful: 'day'}
+                });
+                ctxt.reply();
+            });
+
+            it('should allow providing options', function (done) {
+                var ctxt = new dualapi.MessageContext({
+                    domain: {
+                        send: function (to, from, body, options) {
+                            assert.deepEqual(options, {addendum: 'ps'});
+                            done();
+                        }
+                    }
+                    , from: ['bonjour']
+                });
+                ctxt.reply(null, {addendum: 'ps'});
+            });
+
 
         });
 
         describe('.forward', function () {
 
-            it('should send the argument to the message from host, preserving the from and body fields', function (done) {
+            it('should send to provided "to"', function (done) {
+                var ctxt = new dualapi.MessageContext({
+                    domain: {
+                        send: function (to, from, body) {
+                            assert.deepEqual(to, ['deep', 'space', 'ten']);
+                            done();
+                        }
+                    }
+                    , to: ['deep', 'space', 'twenty']
+                    , from: ['starship', 'enterprise']
+                });
+                ctxt.forward(['deep', 'space', 'ten']);
+            });
+
+            it('should send with original "from" field', function (done) {
                 var ctxt = new dualapi.MessageContext({
                     domain: {
                         send: function (to, from, body) {
                             assert.deepEqual(from, ['starship', 'enterprise']);
-                            assert.deepEqual(to, ['deep', 'space', 'ten']);
-                            assert.deepEqual(body, { borg: 'invasion'});
+                            done();
+                        }
+                    }
+                    , to: ['deep', 'space', 'twenty']
+                    , from: ['starship', 'enterprise']
+                });
+                ctxt.forward(['deep', 'space', 'ten']);
+            });
+
+            it('should send with original "body" field', function (done) {
+                var ctxt = new dualapi.MessageContext({
+                    domain: {
+                        send: function (to, from, body) {
+                            assert.deepEqual(from, ['starship', 'enterprise']);
                             done();
                         }
                     }
@@ -45,6 +112,57 @@ describe('dualapi', function () {
                     , body: { borg: 'invasion'}
                 });
                 ctxt.forward(['deep', 'space', 'ten']);
+            });
+
+            it('should send with original options field', function (done) {
+                var ctxt = new dualapi.MessageContext({
+                    domain: {
+                        send: function (to, from, body, options) {
+                            assert(options);
+                            assert.equal(options.deep, 'space');
+                            done();
+                        }
+                    }
+                    , to: ['deep', 'deeper', 'deepest']
+                    , from: ['something']
+                    , options: {deep: 'space'}
+                });
+                ctxt.forward(['monkey']);
+            });
+
+            it('should send with original additional options field', function (done) {
+
+                var ctxt = new dualapi.MessageContext({
+                    domain: {
+                        send: function (to, from, body, options) {
+                            assert(options);
+                            assert.equal(options.deep, 'space');
+                            assert.equal(options.something, 'completely');
+                            done();
+                        }
+                    }
+                    , to: ['deep', 'deeper', 'deepest']
+                    , from: ['something']
+                    , options: {deep: 'space'}
+                });
+                ctxt.forward(['monkey'], {something: 'completely'});
+            });
+
+            it('should send with overriden options field', function (done) {
+
+                var ctxt = new dualapi.MessageContext({
+                    domain: {
+                        send: function (to, from, body, options) {
+                            assert(options);
+                            assert.equal(options.deep, 'different');
+                            done();
+                        }
+                    }
+                    , to: ['deep', 'deeper', 'deepest']
+                    , from: ['something']
+                    , options: {deep: 'space'}
+                });
+                ctxt.forward(['monkey'], {deep: 'different'});
             });
 
         });
