a5d58dff0628315e01f5489570065c9f469ca21a
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-06-16 15:54:00 +0000

    Satisfy 'forward' tests.

---
+++ src/forward.js
@@ -0,0 +1,16 @@
+/*jslint node: true */
+/* global -Promise */
+"use strict";
+
+var _ = require('lodash');
+
+module.exports = function (Domain) {
+    Domain.prototype.Message.prototype.forward = function (to, options) {
+        var d = this.domain;
+        d.send(to
+               , this.from
+               , this.body
+               , _.extend({}, this.options, options));
+
+    };
+};
