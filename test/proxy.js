606d72c9653d89267c276cde09234cb7b8738940
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-06-17 06:15:24 +0000

    Finish proxy tests.

--- test/proxy.js
+++ test/proxy.js
@@ -34,6 +34,7 @@ describe('proxy', function () {
     it('should return a spreadable promise', function (done) {
         d.mount(['nice'], function (body, ctxt) {
             assert(_.isFunction(ctxt.proxy(['lumberjack']).spread));
+            done();
         });
         d.send(['nice']);
     });
@@ -74,7 +75,7 @@ describe('proxy', function () {
         d.mount(['nice'], function (body, ctxt) {
             ctxt.proxy(['lumberjack'])
                 .spread(function (body, options) {
-                    assert.deepEqual({ read: 'this' }, body);
+                    assert.deepEqual({ read: 'this' }, options);
                     done();
                 })
                 .catch(done);
@@ -100,97 +101,144 @@ describe('proxy', function () {
         d.send(['nice']);
     });
 
-
-    it('should timeout with status code 504 when given a timeout option is provided and the host does not respond', function (done) {
-        d.mount(['cleveland'], function (body, ctxt) {});
-        d.request(['cleveland'], null, { timeout: 0.001 })
-            .spread(function (body, options) {
-                assert.equal(504, options.statusCode);
-                done();
-            })
-            .catch(done);
+    it('should timeout if given timeout option and host does not respond', function (done) {
+        d.mount(['lumberjack'], function (body, ctxt) {});
+        d.mount(['nice'], function (body, ctxt) {
+            ctxt.proxy(['lumberjack'], { timeout: 1 })
+                .spread(function (body, options, respCtxt) {
+                    assert.equal(504, options.statusCode);
+                    done();
+                })
+                .catch(done);
+        });
+        d.send(['nice']);
     });
 
+    it('should timeout if given timeout in original options', function (done) {
+        d.mount(['lumberjack'], function (body, ctxt) {});
+        d.mount(['nice'], function (body, ctxt) {
+            ctxt.proxy(['lumberjack'])
+                .spread(function (body, options, respCtxt) {
+                    assert.equal(504, options.statusCode);
+                    done();
+                })
+                .catch(done);
+        });
+        d.send(['nice'], [], void 0, { timeout: 0.01 });
+    });
 
     it('should have domain in timeout ctxt', function (done) {
-        d.mount(['cleveland'], function (body, ctxt) {});
-        d.request(['cleveland'], null, { timeout: 0.001 })
-            .spread(function (body, options, ctxt) {
-                assert.equal(d, ctxt.domain);
-                done();
-            })
-            .catch(done);
+        d.mount(['lumberjack'], function (body, ctxt) {});
+        d.mount(['nice'], function (body, ctxt) {
+            ctxt.proxy(['lumberjack'], { timeout: 1 })
+                .spread(function (body, options, respCtxt) {
+                    assert.equal(d, respCtxt.domain);
+                    done();
+                })
+                .catch(done);
+        });
+        d.send(['nice']);
     });
 
-    it('should timeout in 1 seconds given a timeout option of 1', function (done) {
-        d.mount(['cleveland'], function (body, ctxt) {});
+    it('should timeout in 1 second if give a timeout option of 1', function (done) {
+        d.mount(['lumberjack'], function (body, ctxt) {});
         var before = Date.now();
-        d.request(['cleveland'], null, { timeout: 1 })
-            .spread(function () {
-                var after = Date.now();
-                assert(after - before >= 1000);
-                assert(after - before < 2000);
-                done();
-            })
-            .catch(done);
+        d.mount(['nice'], function (body, ctxt) {
+            ctxt.proxy(['lumberjack'], { timeout: 1 })
+                .spread(function (body, options, respCtxt) {
+                    var after = Date.now();
+                    assert(after - before >= 1000);
+                    assert(after - before < 2000);
+                    done();
+                })
+                .catch(done);
+        });
+        d.send(['nice']);
     });
 
-        // mocha needs a special option to do this test
-        // it('should timeout in 2 seconds given a timeout option of 2', function (done) {
-        //     d.mount(['cleveland'], function (ctxt) {});
-        //     var before = Date.now();
-        //     d.get(['cleveland'], null, { timeout: 2 })
-        //     .spread(function (ctxt) {
-        //         var after = Date.now();
-        //         assert(after - before > 2000);
-        //         assert(after - before < 3000);
-        //         done();
-        //     });
-        // });
+    it('should timeout faster than original timeout if given smaller timeout ', function (done) {
+        d.mount(['lumberjack'], function (body, ctxt) {});
+        var before = Date.now();
+        d.mount(['nice'], function (body, ctxt) {
+            ctxt.proxy(['lumberjack'], { timeout: 0.1 })
+                .spread(function (body, options, respCtxt) {
+                    var after = Date.now();
+                    assert(after - before < 1000);
+                    done();
+                })
+                .catch(done);
+        });
+        d.send(['nice'], [], void 0, { timeout: 1.5});
+    });
+
+    it('should timeout faster than proxy timeout if original is smaller ', function (done) {
+        d.mount(['lumberjack'], function (body, ctxt) {});
+        var before = Date.now();
+        d.mount(['nice'], function (body, ctxt) {
+            ctxt.proxy(['lumberjack'], { timeout: 1.5 })
+                .spread(function (body, options, respCtxt) {
+                    var after = Date.now();
+                    assert(after - before < 1000);
+                    done();
+                })
+                .catch(done);
+        });
+        d.send(['nice'], [], void 0, { timeout: 0.1});
+    });
 
     it('should not timeout when given a timeout option of 0', function (done) {
-        d.mount(['cleveland'], function (body, ctxt) {});
-        d.request(['cleveland'], null, { timeout: 0 })
-        .then(function () {
-            done('unexpected return');
-        })
-            .catch(done);
+        d.mount(['lumberjack'], function (body, ctxt) {});
+        var before = Date.now();
+        d.mount(['nice'], function (body, ctxt) {
+            ctxt.proxy(['lumberjack'], { timeout: 0 })
+                .spread(function () {
+                    done('unexpected return');
+                })
+                .catch(done);
+        });
+        d.send(['nice']);
         done();
     });
 
     it('should return unavailable statusCode 503 when no host matches', function (done) {
-        d.request(['cleveland'])
-            .spread(function (body, options) {
-                assert.equal(503, options.statusCode);
-                done();
-            })
-            .catch(done);
+        d.mount(['nice'], function (body, ctxt) {
+            ctxt.proxy(['lumberjack'])
+                .spread(function (body, options, ctxt) {
+                    assert.equal(503, options.statusCode);
+                    done();
+                })
+                .catch(done);
+        });
+        d.send(['nice']);
     });
 
     it('should not leak request listeners', function (done) {
-        d.mount(['dernier'], function (body, ctxt) {
+        d.mount(['lumberjack'], function (body, ctxt) {
             ctxt.send(ctxt.from, [], 'voyage');
         });
-        var initialCount = d.listeners('**').length;
-        d.request(['dernier'], 'last')
-            .spread(function (body) {
-                assert.equal('voyage', body);
-                assert.equal(initialCount, d.listeners('**').length);
-                done();
-            })
-            .catch(done);
+        d.mount(['nice'], function (body, ctxt) {
+            var initialCount = d.listeners('**').length;
+            ctxt.proxy(['lumberjack'])
+                .spread(function () {
+                    assert.equal(initialCount, d.listeners('**').length);
+                    done();
+                })
+                .catch(done);
+        });
+        d.send(['nice']);
     });
 
-    it('should not leak listeners on timeout ', function (done) {
-        d.mount(['cleveland'], function (body, ctxt) {});
-        var initialCount = d.listeners('**').length;
-        d.request(['cleveland'], null, { timeout: 0.001 })
-            .spread(function (body, options) {
-                assert.equal(initialCount, d.listeners('**').length);
-                done();
-            })
-            .catch(done);
+    it('should not leak request listeners on timeout', function (done) {
+        d.mount(['nice'], function (body, ctxt) {
+            var initialCount = d.listeners('**').length;
+            ctxt.proxy(['lumberjack'], { timeout: 0.001 })
+                .spread(function () {
+                    assert.equal(initialCount, d.listeners('**').length);
+                    done();
+                })
+                .catch(done);
+        });
+        d.send(['nice']);
     });
-
 
 });
