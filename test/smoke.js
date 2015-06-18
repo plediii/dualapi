e2dbe6f7e48e839b9d283816535f3ea5bdbe1026
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-06-18 05:18:50 +0000

    Add smoke test.

---
+++ test/smoke.js
@@ -0,0 +1,65 @@
+/*jslint node: true */
+"use strict";
+
+var _ = require('lodash');
+var assert = require('assert');
+
+var dualapi = require('../index');
+
+
+describe('dualapi', function () {
+
+    var d;
+    beforeEach(function () {
+        d = dualapi();
+    });
+
+    describe('smoke test', function () {
+
+        it('should be constructable', function () {
+            assert(d);
+        });
+
+    });
+    
+    describe('domain attributes', function () {
+
+    
+        it('should have request', function () {
+            assert(_.isFunction(d.request));
+        });
+
+    });
+
+    describe('domain attributes', function () {
+
+        
+        var m;
+        beforeEach(function (done) {
+            d.mount(['toot'], function (body, ctxt) {
+                m = ctxt;
+                done();
+            });
+            d.send(['toot']);
+        });
+
+    
+        it('should have request', function () {
+            assert(_.isFunction(m.request));
+        });
+
+        it('should have forward', function () {
+            assert(_.isFunction(m.forward));
+        });
+
+        it('should have return', function () {
+            assert(_.isFunction(m.return));
+        });
+
+        it('should have proxy', function () {
+            assert(_.isFunction(m.proxy));
+        });
+
+    });
+
+});
