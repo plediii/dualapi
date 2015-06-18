43538224fe83e0213f642c90327ab6b6b1e52a33
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-06-18 05:19:03 +0000

    Satisfy smoke tests.

---
+++ src/dualapi.js
@@ -0,0 +1,10 @@
+/*jslint node: true */
+/* global -Promise */
+"use strict";
+
+module.exports = function (Domain) {
+    require('./forward')(Domain);
+    require('./proxy')(Domain);
+    require('./request')(Domain);
+    require('./return')(Domain);
+};
