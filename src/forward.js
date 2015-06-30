24e1d2d1f6d27e5549b04a5d048832e88bc02ade
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-06-30 04:43:40 +0000

    Expect lodash to be provided in constructor libs.

--- src/forward.js
+++ src/forward.js
@@ -2,9 +2,8 @@
 /* global -Promise */
 "use strict";
 
-var _ = require('./lodash');
-
-module.exports = function (Domain) {
+module.exports = function (Domain, libs) {
+    var _ = libs._;
     Domain.prototype.Message.prototype.forward = function (to, options) {
         var d = this.domain;
         d.send(to
